250beb8c871ea2e760da000e76453e362e135f01
heng30 <2238288979@qq.com>
2025-08-04 23:58:48 +0800

    [*] update normally

--- tasklog/src/logic/statistic.rs
+++ tasklog/src/logic/statistic.rs
@@ -1,14 +1,15 @@
-use super::{toast, tr::tr};
+use super::tr::tr;
 use crate::{
     db::{
         self,
         def::{RecordEntry, ARCHIVE_TABLE, RECORD_TABLE},
     },
-    slint_generatedAppWindow::{AppWindow, Logic, RecordEntry as UIRecordEntry, Store},
-    toast_success, toast_warn,
+    slint_generatedAppWindow::{
+        AppWindow, ChartBarEntry as UIChartBarEntry, Logic, RecordEntry as UIRecordEntry,
+        RecordState as UIRecordState, Store,
+    },
 };
-use anyhow::Result;
-use slint::{ComponentHandle, Model, ModelRc, VecModel, Weak};
+use slint::{Brush::SolidColor, ComponentHandle, Model, ModelRc, VecModel};
 
 #[macro_export]
 macro_rules! store_statistic_entries {
@@ -28,7 +29,6 @@ pub fn init(ui: &AppWindow) {
         statistic_init(&ui);
     });
 
-    // let ui_handle = ui.as_weak();
     ui.global::<Logic>()
         .on_statistic_total_days_spent(move |entries| {
             let mut days = 0;
@@ -58,10 +58,238 @@ pub fn init(ui: &AppWindow) {
             return days as i32;
         });
 
-    // let ui_handle = ui.as_weak();
-    // ui.global::<Logic>().on_mean_days_spent(move |entries| {
-    //     let ui = ui_handle.unwrap();
-    // });
+    let ui_handle = ui.as_weak();
+    ui.global::<Logic>()
+        .on_statistic_chart_tasks_count(move |entries| {
+            let ui = ui_handle.unwrap();
+
+            let mut items = vec![
+                UIChartBarEntry {
+                    label: tr("NotStarted").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::NotStarted),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Running").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Running),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Finished").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Finished),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Giveup").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Giveup),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Timeout").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Timeout),
+                    ),
+                },
+            ];
+
+            for entry in entries.iter() {
+                let index = match entry.state {
+                    UIRecordState::NotStarted => 0,
+                    UIRecordState::Running => 1,
+                    UIRecordState::Finished => 2,
+                    UIRecordState::Giveup => 3,
+                    UIRecordState::Timeout => 4,
+                };
+
+                items[index].value += 1;
+            }
+
+            ModelRc::new(VecModel::from_slice(&items))
+        });
+
+    let ui_handle = ui.as_weak();
+    ui.global::<Logic>()
+        .on_statistic_chart_days_spent(move |entries| {
+            let ui = ui_handle.unwrap();
+
+            let mut items = vec![
+                UIChartBarEntry {
+                    label: tr("Running").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Running),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Finished").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Finished),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Giveup").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Giveup),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Timeout").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Timeout),
+                    ),
+                },
+            ];
+
+            let current_timestamp = cutil::time::timestamp();
+            let day_seconds = 24 * 60 * 60;
+
+            for entry in entries.iter() {
+                if entry.state == UIRecordState::NotStarted {
+                    continue;
+                }
+
+                let start_timestamp = cutil::time::date_str_to_timestamp(&entry.start_date);
+                let end_timestamp = cutil::time::date_str_to_timestamp(&entry.end_date);
+                if start_timestamp.is_err() || end_timestamp.is_err() {
+                    continue;
+                }
+
+                let start_timestamp = start_timestamp.unwrap();
+                let end_timestamp = end_timestamp.unwrap();
+                if current_timestamp < start_timestamp {
+                    continue;
+                }
+
+                let days = if current_timestamp < end_timestamp {
+                    (current_timestamp - start_timestamp) / day_seconds
+                } else {
+                    (end_timestamp - start_timestamp) / day_seconds
+                };
+
+                let index = match entry.state {
+                    UIRecordState::Running => 0,
+                    UIRecordState::Finished => 1,
+                    UIRecordState::Giveup => 2,
+                    UIRecordState::Timeout => 3,
+                    _ => unreachable!(),
+                };
+
+                items[index].value += days as i32;
+            }
+
+            ModelRc::new(VecModel::from_slice(&items))
+        });
+
+    let ui_handle = ui.as_weak();
+    ui.global::<Logic>()
+        .on_statistic_chart_mean_days_spent(move |entries| {
+            let ui = ui_handle.unwrap();
+
+            let mut counts = vec![0; 4];
+            let mut items = vec![
+                UIChartBarEntry {
+                    label: tr("Running").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Running),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Finished").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Finished),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Giveup").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Giveup),
+                    ),
+                },
+                UIChartBarEntry {
+                    label: tr("Timeout").into(),
+                    value: 0,
+                    color: SolidColor(
+                        ui.global::<Logic>()
+                            .invoke_state_color(UIRecordState::Timeout),
+                    ),
+                },
+            ];
+
+            let current_timestamp = cutil::time::timestamp();
+            let day_seconds = 24 * 60 * 60;
+
+            for entry in entries.iter() {
+                if entry.state == UIRecordState::NotStarted {
+                    continue;
+                }
+
+                let start_timestamp = cutil::time::date_str_to_timestamp(&entry.start_date);
+                let end_timestamp = cutil::time::date_str_to_timestamp(&entry.end_date);
+                if start_timestamp.is_err() || end_timestamp.is_err() {
+                    continue;
+                }
+
+                let start_timestamp = start_timestamp.unwrap();
+                let end_timestamp = end_timestamp.unwrap();
+                if current_timestamp < start_timestamp {
+                    continue;
+                }
+
+                let days = if current_timestamp < end_timestamp {
+                    (current_timestamp - start_timestamp) / day_seconds
+                } else {
+                    (end_timestamp - start_timestamp) / day_seconds
+                };
+
+                let index = match entry.state {
+                    UIRecordState::Running => 0,
+                    UIRecordState::Finished => 1,
+                    UIRecordState::Giveup => 2,
+                    UIRecordState::Timeout => 3,
+                    _ => unreachable!(),
+                };
+
+                counts[index] += 1;
+                items[index].value += days as i32;
+            }
+
+            for index in 0..4 {
+                if counts[index] <= 0 {
+                    continue;
+                }
+
+                items[index].value = items[index].value / counts[index];
+            }
+
+            ModelRc::new(VecModel::from_slice(&items))
+        });
 }
 
 fn statistic_init(ui: &AppWindow) {
